07b5ee0b03aa7bc7e3fc78b5d8584d782f48a591
Ricardo Silva <ricardo.garcia.silva@gmail.com>
2011-08-19 17:04:40 +0100

    Changed code style to reflect PEP8 guidelines and started adding ConfigParser. The code is unusable at the moment.

--- sshlist.py
+++ sshlist.py
@@ -7,93 +7,100 @@ import appindicator
 import os
 from subprocess import Popen, PIPE
 import pynotify
+import logging
+import configParser
+
+logging.basicConfig(level=logging.DEBUG)
 
 # TODO
 # - Change all the spacing, variable and function names to conform to PEP8
-# - Finish the runProgram (soon to be renamed run_program) method's new 
+# - Finish the run_program (soon to be renamed run_program) method's new 
 # implementation, using the subprocess.Popen class, instead of the old
 # os.popen
-# Review the rest of the code
+# - Review the rest of the code
+# - Use python's configParser module to store the settings
+# - add logging
 
-class sshList:
-    ver = "0.2"
-    list_path = os.path.join(os.getenv("HOME"), ".sshlist")
-
+class SSHList:
+    version = "0.2"
+    listPath = os.path.join(os.getenv("HOME"), ".sshlist")
 
     def __init__(self):
-        self.checkFile()
-        self.buildIndicator()
-        self.refreshMenu()
+        self.check_file()
+        self.build_indicator()
+        self.refresh_menu()
         gtk.main()
 
+    def check_file(self):
+        if not os.path.exists(self.listPath):
+            open(self.listPath, 'w').close()
 
-    def checkFile(self):
-        if not os.path.exists(self.list_path):
-            open(self.list_path, 'w').close()
-
-
-    def buildIndicator(self):
-        self.ind = appindicator.Indicator("sshlist",
+    def build_indicator(self):
+        self.indicator = appindicator.Indicator("sshlist",
             "gnome-netstatus-tx",
             appindicator.CATEGORY_APPLICATION_STATUS)
-        self.ind.set_label("SSH")
-        self.ind.set_status(appindicator.STATUS_ACTIVE)
-        self.ind.set_attention_icon("connect_creating")
+        self.indicator.set_label("SSH")
+        self.indicator.set_status(appindicator.STATUS_ACTIVE)
+        self.indicator.set_attention_icon("connect_creating")
 
+    #def run_program(self, command):
+    #    cmd = "gnome-terminal -x ssh " + command
+    #    #returns (output, exit value)
+    #    fd=os.popen(cmd, "r")
+    #    output=fd.read()
+    #    exitvalue=fd.close()
+    #    return (output, exitvalue)
 
-    def runProgram(self, command):
-        cmd = "gnome-terminal -x ssh " + command
-        #returns (output, exit value)
-        fd=os.popen(cmd, "r")
-        output=fd.read()
-        exitvalue=fd.close()
-        return (output, exitvalue)
-
-    def runProgram(self, program='gnome-terminal', progOptions='-x', 
-                   sshCommand='ssh', sshOptions='-Y', otherOptions=''):
+    def run_program(self, sshHost, sshUser, sshPassword=None, 
+                    program='gnome-terminal', progOptions='-x', 
+                    sshCommand='ssh', sshOptions='-Y', otherOptions=''):
+        sshLogin = '@'.join(sshUser, sshHost)
         otherOpts = otherOptions.split()
-        cmdList = [program, progOptions, sshCommand, sshOptions] + otherOpts
-        neWProcess = Popen(cmdList, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        # work-in-progress...
+        cmdList = [program, progOptions, sshCommand, sshOptions, sshLogin] + \
+                  otherOpts
+        newProcess = Popen(cmdList, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = newProcess.communicate()
+        returnCode = newProcess.returncode
+        return stdout, returnCode
 
-
-
-    def menuitemResponse(self, widget, command):
+    def menu_item_response(self, widget, command):
         if command == "_about":
             md = gtk.MessageDialog(None, 0, gtk.MESSAGE_INFO,
             gtk.BUTTONS_OK)
-            md.set_markup("<b>sshlist v%s</b>" % self.ver)
-            md.format_secondary_markup("""A simple sshmenu like replacement for appindicator menu.
+            md.set_markup("<b>sshlist v%s</b>" % self.version)
+            md.format_secondary_markup(
+            """
+            A simple sshmenu like replacement for appindicator menu.
+            Author: anil.verve@gmail.com
+            http://www.gulecha.org
+            Updated: phoolish@gmail.com
 
-Author: anil.verve@gmail.com
-http://www.gulecha.org
-Updated: phoolish@gmail.com
+            # Instructions
 
-# Instructions
-
-1. Copy file sshlist.py to /usr/local/bin
-2. Launch sshlist.py
-3. Or better yet, add it to gnome startup programs list so it's run on login.""")
+            1. Copy file sshlist.py to /usr/local/bin
+            2. Launch sshlist.py
+            3. Or better yet, add it to gnome startup programs list so it's run on login.
+            """)
             md.run()
             md.destroy()
         elif command == "_settings":
-            self.settingsDialogue()
+            self.settings_dialogue()
         else:
-            self.runProgram(command)
+            self.run_program(command)
 
 
-    def settingsDialogue(self):
+    def settings_dialogue(self):
         settings = gtk.Window(gtk.WINDOW_TOPLEVEL)
         settings.set_title("Settings")
         settings.set_position(gtk.WIN_POS_CENTER)
         settings.set_border_width(5)
 
         add_ssh = gtk.Button("Add")
-        add_ssh.connect_object("clicked", self.addHost, settings)
+        add_ssh.connect_object("clicked", self.add_host, settings)
         save_ssh = gtk.Button("Save")
-        save_ssh.connect_object("clicked", self.saveList, settings)
+        save_ssh.connect_object("clicked", self.save_list, settings)
         delete_ssh = gtk.Button("Delete")
-        delete_ssh.connect_object("clicked", self.deleteHost, settings)
+        delete_ssh.connect_object("clicked", self.delete_host, settings)
         close_ssh = gtk.Button("Close")
         close_ssh.connect_object("clicked", gtk.Widget.destroy, settings)
 
@@ -102,18 +109,18 @@ Updated: phoolish@gmail.com
         tree_store = gtk.TreeStore(str, str)
 
         # read in the ssh hosts list from ~/.sshlist
-        hosts = open(self.list_path, "r").read()
+        hosts = open(self.listPath, "r").read()
         hostlist = hosts.split("\n")
 
         # create some
         newIter = None
         tempIter = None
-        for hostinfo in hostlist:
-            # if hostinfo is not a comment
-            if not hostinfo.startswith("#") and hostinfo != "":
-                hostparts = hostinfo.split(":::")
-                title = hostinfo
-                command = hostinfo
+        for hostInfo in hostlist:
+            # if hostInfo is not a comment
+            if not hostInfo.startswith("#") and hostInfo != "":
+                hostparts = hostInfo.split(":::")
+                title = hostInfo
+                command = hostInfo
                 if len(hostparts) > 1:
                     #second is ssh command
                     command = hostparts.pop()
@@ -136,7 +143,7 @@ Updated: phoolish@gmail.com
 
         host_cell = gtk.CellRendererText()
         host_cell.set_property('editable', True)
-        host_cell.connect('edited', self.hostEdit, (tree_store, 0))
+        host_cell.connect('edited', self.host_edit, (tree_store, 0))
         host_tree_view_column.pack_start(host_cell, True)
         host_tree_view_column.add_attribute(host_cell, 'text', 0)
 
@@ -145,7 +152,7 @@ Updated: phoolish@gmail.com
         self.tree_view.append_column(command_tree_view_column)
         command_cell = gtk.CellRendererText()
         command_cell.set_property('editable', True)
-        command_cell.connect('edited', self.hostEdit, (tree_store, 1))
+        command_cell.connect('edited', self.host_edit, (tree_store, 1))
         command_tree_view_column.pack_start(command_cell, True)
         command_tree_view_column.add_attribute(command_cell, 'text', 1)
         table.attach(self.tree_view, 0, 1, 0, 3)
@@ -159,89 +166,90 @@ Updated: phoolish@gmail.com
         settings.show_all()
 
 
-    def addHost(self, widget, data = None):
-        tree_model = self.tree_view.get_model()
-        newIter = tree_model.append(None, ['', ''])
+    def add_host(self, widget, data = None):
+        treeModel = self.tree_view.get_model()
+        newIter = treeModel.append(None, ['', ''])
         self.selection.select_iter(newIter)
 
-    def deleteHost(self, widget, data = None):
+    def delete_host(self, widget, data = None):
         dialog = gtk.MessageDialog(
             message_format='Are you sure to delete this?',
             buttons=gtk.BUTTONS_OK_CANCEL)
         response = dialog.run()
         if response == gtk.RESPONSE_OK:
-            tree_model, tree_iter = self.selection.get_selected()
-            tree_model.remove(tree_iter)
-            self.saveList()
+            treeModel, treeIter = self.selection.get_selected()
+            treeModel.remove(treeIter)
+            self.save_list()
         dialog.destroy()
 
-    def saveList(self, widget = None):
+    def save_list(self, widget = None):
             # Overwrite sshlist file
-            ssh_file = open(self.list_path, "w")
-            ssh_file.write("# Add ssh host info or add a title:::host info")
-            ssh_file.write("# Examples\n")
-            ssh_file.write("# user@host.com\n")
-            ssh_file.write("# title:::user@host.com\n")
+            sshFile = open(self.listPath, "w")
+            sshFile.write("# Add ssh host info or add a title:::host info")
+            sshFile.write("# Examples\n")
+            sshFile.write("# user@host.com\n")
+            sshFile.write("# title:::user@host.com\n")
 
-            tree_model = self.tree_view.get_model()
-            for hostIter in tree_model:
+            treeModel = self.tree_view.get_model()
+            for hostIter in treeModel:
                 name, command = hostIter
-                ssh_file.write(name + ":::" + command + "\n")
+                sshFile.write(name + ":::" + command + "\n")
 
-            ssh_file.close()
-            self.refreshMenu()
+            sshFile.close()
+            self.refresh_menu()
             pynotify.Notification("Updated sshlist", "Menu list was refreshed from ~/.sshlist").show()
 
-    def hostEdit(self, cell, path, new_text, user_data):
-            liststore, column = user_data
-            liststore[path][column] = new_text
+    def host_edit(self, cell, path, new_text, user_data):
+            listStore, column = user_data
+            listStore[path][column] = new_text
             return
 
     #delete line
-    def refreshMenu(self):
-            self.buildMenu()
-            self.ind.set_menu(self.menu)
+    def refresh_menu(self):
+            self.build_menu()
+            self.indicator.set_menu(self.menu)
 
-    def buildMenu(self):
+    def build_menu(self):
         # create a menu
         self.menu = gtk.Menu()
 
         # read in the ssh hosts list from ~/.sshlist
-        hosts = open(self.list_path, "r").read()
+        hosts = open(self.listPath, "r").read()
         hostlist = hosts.split("\n")
 
         # create some
-        for hostinfo in hostlist:
+        for hostInfo in hostlist:
             # if it isn't a comment create a menu item
-            if not hostinfo.startswith("#") and hostinfo != "":
-                hostparts = hostinfo.split(":::")
-                title = hostinfo
-                command = hostinfo
+            if not hostInfo.startswith("#") and hostInfo != "":
+                #hostparts = hostInfo.split(":::")
+                title, command = hostInfo.split(':::')
+                #title = hostInfo
+                #command = hostInfo
                 if len(hostparts) > 1:
                     #second is ssh command
                     command = hostparts.pop()
                     #first section is title
                     title = hostparts.pop()
-                menu_items = gtk.MenuItem(title)
-                self.menu.append(menu_items)
+                menuItems = gtk.MenuItem(title)
+                self.menu.append(menuItems)
                 # this is where you would connect your menu item up with a function:
-                menu_items.connect("activate", self.menuitemResponse, command)
+                menuItems.connect("activate", self.menu_item_response, command)
                 # show the items
-                menu_items.show()
+                menuItems.show()
 
         separator = gtk.SeparatorMenuItem()
         separator.show()
         self.menu.append(separator)
 
-        menu_items = gtk.MenuItem("Settings")
-        menu_items.connect("activate", self.menuitemResponse, "_settings")
-        menu_items.show()
-        self.menu.append(menu_items)
+        menuItems = gtk.MenuItem("Settings")
+        menuItems.connect("activate", self.menu_item_response, "_settings")
+        menuItems.show()
+        self.menu.append(menuItems)
 
-        menu_items = gtk.MenuItem("About")
-        menu_items.connect("activate", self.menuitemResponse, "_about")
-        menu_items.show()
-        self.menu.append(menu_items)
+        menuItems = gtk.MenuItem("About")
+        menuItems.connect("activate", self.menu_item_response, "_about")
+        menuItems.show()
+        self.menu.append(menuItems)
 
         quit_item = gtk.MenuItem("Quit")
         quit_item.connect("activate", gtk.main_quit, None)
